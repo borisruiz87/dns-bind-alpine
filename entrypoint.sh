ce54270bf2d64e12cb8b96360323efc5a2db720b
borisruiz87 <86937980+borisruiz87@users.noreply.github.com>
2023-07-09 14:55:16 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,6 @@
-#!/bin/bash
-
-echo "Comprobando el nombre del script"
-echo $0
+#!/bin/ash
 
 if [[ $(ls /var/dns/ | wc -l) == 0 ]]
 then
     cp /var/tmp/* /var/dns/
-    echo "TMP tiene:"
-    echo $(ls -lah /var/tmp/) 
 fi
-
-echo "Comprobando ultimo ejecutado script"
-echo $?
-echo "Completado script"
-
